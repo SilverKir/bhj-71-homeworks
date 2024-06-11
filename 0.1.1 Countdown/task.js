1597811575baf95c78c2fb7ca3a684ace70dfadd
silver_kir <rci@mp03.ru>
2024-06-11 09:05:38 +0800

    second task done

---
+++ 0.1.1 Countdown/task.js
@@ -0,0 +1,56 @@
+
+const startTimer = document.getElementById("seconds").textContent;
+const startMinutes = document.getElementById("minutes").textContent;
+const startHours = document.getElementById("hours").textContent;
+
+const timerResult = () => {
+    alert("Вы победили в конкурсе!");
+}
+
+
+const decreaseTimer = (param, paramDidit) => {
+    output = document.getElementById(param)
+    output.textContent -= 1;
+    if (output.textContent == 9) {
+        document.getElementById(paramDidit).textContent = 0
+    }
+}
+
+const setTimeData = (param, paramValue, paramDigit) => {
+    if (paramValue > 9) {
+        document.getElementById(paramDigit).textContent = '';
+    } else {
+        document.getElementById(paramDigit).textContent = 0;
+    }
+    document.getElementById(param).textContent = + paramValue;
+}
+
+const setInitialTimeData = () => {
+    setTimeData("hours", startHours, "hoursZero");
+    setTimeData("minutes", startMinutes, "minutesZero");
+    setTimeData("seconds", startTimer, "secondsZero");
+
+}
+
+const countdownStart = () => {
+    if (document.getElementById("seconds").textContent > 0) {
+        decreaseTimer("seconds", "secondsZero")
+    } else {
+        if ((document.getElementById("minutes").textContent > 0)) {
+            setTimeData("seconds", 60, "secondsZero");
+            decreaseTimer("minutes", "minutesZero");
+        } else {
+            if ((document.getElementById("hours").textContent > 0)) {
+                setTimeData("minutes", 60, "minutesZero");
+                decreaseTimer("hours", "hoursZero");
+            } else {
+                setInitialTimeData();
+                timerResult();
+            }
+        }
+    }
+
+}
+
+setInitialTimeData();
+setInterval(countdownStart, 1000);
